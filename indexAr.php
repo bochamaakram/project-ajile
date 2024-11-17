dd261c833f2fecc6c350bd9c0e70513347da133d
Akram <akrambouchama0@gmail.com>
2024-11-17 15:09:50 +0100

    fixing bugs

--- indexAr.php
+++ indexAr.php
@@ -150,11 +150,11 @@ body{
                       <ul class="navbar-nav"> 
                           <li class="nav-item dropdown">
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="quran.php" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: black;">القرآن</a>
+                                <a class="nav-link dropdown-toggle" href="quranAr.php" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: black;">القرآن</a>
                                 <ul class="dropdown-menu">
-                                  <li><a class="dropdown-item" href="quran.php#audio">تسجيل الصوت</a></li>
-                                  <li><a class="dropdown-item" href="quran.php#juze">أجزاء القرآن</a></li>
-                                  <li><a class="dropdown-item" href="quran.php#sura">سور القرآن</a></li>
+                                  <li><a class="dropdown-item" href="quranAr.php#audio">تسجيل الصوت</a></li>
+                                  <li><a class="dropdown-item" href="quranAr.php#juze">أجزاء القرآن</a></li>
+                                  <li><a class="dropdown-item" href="quranAr.php#sura">سور القرآن</a></li>
                                 </ul>
                               </li>
                           <li class="nav-item d-flex justify-content-center "><a style="color:black;" class="nav-link" href="quranaudioAr.php">القرآن الصوتي</a></li>
